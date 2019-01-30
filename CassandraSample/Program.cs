8972f5c4e767334e4e227164eae19144a73cc3b3
makbari.it <makbari.it@outlook.com>
2019-01-30 09:59:44 +0330

    init

---
+++ CassandraSample/Program.cs
@@ -0,0 +1,127 @@
+﻿using Cassandra;
+using System;
+using Cassandra.Data.Linq;
+using Cassandra.Mapping;
+
+namespace CassandraSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cluster = Cluster.Builder()
+                     .AddContactPoints("172.26.146.247")
+                     //.AddContactPoints("192.168.20.201")
+                     //.AddContactPoints("192.168.20.203")
+                     .WithCredentials("cassandra", "cassandra")
+                     .Build();
+                        MappingConfiguration.Global.Define<MyMappings>();
+
+            var session = cluster.Connect("space");
+
+            //Linq(session);
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    InsertLinq(session);
+            //}
+
+
+            //UpdateById(session);
+
+
+            //var users = new Table<User>(session);
+
+            //var id = TimeUuid.Parse("6ab09bec-e68e-48d9-a5f8-97e6fb4c9b47");
+
+            //users.Where(u => u.FristName == "xyz")
+            //  .Select(u => new User { LastName = "abc" })
+            //  .Update()
+            //  .Execute();
+
+            PrintCountCql(session);
+
+           // PrintListCql(session);
+
+            Console.ReadLine();
+        }
+
+        private static void UpdateById(ISession session)
+        {
+            var users = new Table<User>(session);
+
+            var id = TimeUuid.Parse("6ab09bec-e68e-48d9-a5f8-97e6fb4c9b47");
+
+            users.Where(u => u.Id == id)
+              .Select(u => new User { FristName = "xyz" })
+              .Update()
+              .Execute();
+        }
+
+        private static void InsertLinq(ISession session)
+        {
+            var users = new Table<User>(session);
+
+            var random = new Random();
+            var number = random.Next(1, 10000);
+
+            users.Insert(new User
+            {
+                Id = TimeUuid.NewId(),
+                FristName = $"f {number}",
+                LastName = $"l {number}"
+            }).Execute();
+        }
+
+        private static void Linq(ISession session)
+        {
+            var users = new Table<User>(session);
+
+            User user = (from x in users select x).FirstOrDefault().Execute();
+        }
+
+        private static void PrintListCql(ISession session)
+        {
+            var result = session.Execute("SELECT id, firstname, lastname FROM user");
+
+            foreach (var row in result)
+            {
+                var id = row.GetValue<Guid>("id");
+                var lastName = row.GetValue<string>("lastname");
+                var firstName = row.GetValue<string>("firstname");
+                Console.WriteLine($"{id} - {firstName} - {lastName}");
+            }
+        }
+
+        private static void PrintCountCql(ISession session)
+        {
+            var result = session.Execute("SELECT COUNT(*) FROM user");
+
+            foreach (var row in result)
+            {
+                var count = row.GetValue<long>("count");
+                Console.WriteLine($"Count: {count}");
+            }
+        }
+    }
+
+    public class MyMappings : Mappings
+    {
+        public MyMappings()
+        {
+            For<User>()
+               .TableName("user")
+               .PartitionKey(u => u.Id)
+               .Column(u => u.Id, cm => cm.WithName("id"))
+               .Column(c=>c.FristName, cm=> cm.WithName("firstname"))
+               .Column(c => c.LastName, cm => cm.WithName("lastname"));
+        }
+    }
+
+    public class User
+    {
+        public TimeUuid Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ KafkaSample/Program.cs
@@ -0,0 +1,93 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace KafkaSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "172.26.146.243:9092",
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            using (var consumer = new Consumer<Ignore, string>(conf))
+            {
+                consumer.Subscribe("TutorialTopic");
+
+                bool consuming = true;
+                consumer.OnError += (_, e) => consuming = !e.IsFatal;
+
+                while (consuming)
+                {
+                    try
+                    {
+                        var cr = consumer.Consume();
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+
+                consumer.Close();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ProducerX()
+        {
+            var config = new ProducerConfig { BootstrapServers = "172.26.146.243:9092" };
+
+            using (var producer = new Producer<Null, string>(config))
+            {
+                try
+                {
+                    var dr = producer.ProduceAsync("TutorialTopic", new Message<Null, string> { Value = "test" }).GetAwaiter().GetResult();
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (KafkaException e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+
+
+    //public interface IBookingConsumer
+    //{
+    //    void Listen(Action<string> message);
+    //}
+
+    //public class BookingConsumer : IBookingConsumer
+    //{
+    //    public void Listen(Action<string> message)
+    //    {
+    //        var config = new Dictionary<string, object>
+    //        {
+    //            { "group.id","booking_consumer" },
+    //            { "bootstrap.servers", "172.26.146.243:9092" },
+    //            { "enable.auto.commit", "false" }
+    //        };
+
+    //        using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+    //        {
+    //            consumer.Subscribe("TutorialTopic");
+    //            consumer.OnMessage += (_, msg) => {
+    //                message(msg.Value);
+    //            };
+
+    //            while (true)
+    //            {
+    //                consumer.Poll(100);
+    //            }
+    //        }
+    //    }
+    //}
+}
